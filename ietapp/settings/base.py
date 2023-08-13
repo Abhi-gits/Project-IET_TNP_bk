ea41decedcce77fd48641dce711ce3d29b6257d4
Abhishek Gupta <ravi37463@gmail.com>
2023-08-13 16:04:02 +0530

    updated setting

--- ietapp/settings/base.py
+++ ietapp/settings/base.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config("SECRET_KEY", default="django-insecure$ietapp.settings.local")
 
-DEBUG = config("DEBUG", default=True, cast=bool)
+DEBUG = config("DEBUG", default=False, cast=bool)
 
 ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="127.0.0.1,localhost", cast=Csv())
 
