6d97199076022345fa3bf9e8740152bdfc5c5d90
anurag629 <akvermaav629@gmail.com>
2023-10-09 11:52:44 +0530

    fixed the errors

--- api/tnp/admin.py
+++ api/tnp/admin.py
@@ -6,18 +6,18 @@ from api.tnp.models import Courses
 
 # Register your models here.
 class PlacementAdmin(admin.ModelAdmin):
-    list_display = ['id', 'user', 'company_name', 'company_email', 'company_salary', 'company_location', 'company_category', 'company_status', 'is_approved']
-    list_filter = ['user', 'company_name', 'company_email',  'company_salary', 'company_location', 'company_category', 'company_status',  'is_approved']
-    search_fields = ['user', 'company_name', 'company_email', 'company_salary', 'company_location', 'company_category', 'company_status',  'is_approved']
+    list_display = ['id', 'user', 'name', 'company_name', 'company_email', 'company_salary', 'is_approved']
+    list_filter = ['user', 'name', 'company_name', 'company_email',  'company_salary', 'is_approved']
+    search_fields = ['user', 'name', 'company_name', 'company_email', 'company_salary', 'is_approved']
     list_per_page = 20
 
 admin.site.register(Placement, PlacementAdmin)
 
 
 class CoursesAdmin(admin.ModelAdmin):
-    list_display = ['id', 'user', 'course_name', 'course_status', 'is_approved']
-    list_filter = ['user', 'course_name', 'course_status', 'is_approved']
-    search_fields = ['user', 'course_name', 'course_status', 'is_approved']
+    list_display = ['id', 'user', 'name', 'course_name', 'is_approved']
+    list_filter = ['user', 'name', 'course_name', 'is_approved']
+    search_fields = ['user', 'name', 'course_name', 'is_approved']
     list_per_page = 20
 
 admin.site.register(Courses, CoursesAdmin)
